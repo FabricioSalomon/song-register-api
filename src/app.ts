1cc5664c4c9330056aa07aa6393f15a88518b11e
Fabricio Salomon <fabriciosalomon14@gmail.com>
2024-04-16 09:37:30 -0300

    feat: add knex database connection

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,24 @@
-import express from 'express';
-import type { Request, Response } from 'express';
+import body_parser from 'body-parser';
+import cors, { type CorsOptions } from 'cors';
+import express, { type Request, type Response } from 'express';
+
+import { knex } from './database';
 
 const app = express();
-const port = 3333;
+const port = 3306;
 
-app.get('/health-check', (req: Request, res: Response) => {
+const cors_options: CorsOptions = {
+	origin: `http://localhost:/${port}`
+};
+
+app.use(cors(cors_options));
+app.use(body_parser.json());
+app.use(body_parser.urlencoded({ extended: true }));
+
+app.get('/health-check', async (req: Request, res: Response) => {
+	const tables = await knex('sqlite_schema').select('*');
+
+	console.log({ tables });
 	res.status(200).json({
 		message: 'Server running!'
 	});
